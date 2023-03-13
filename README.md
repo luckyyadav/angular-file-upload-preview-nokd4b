3ee34a19e4506005892146418d481cf36d805ee5
luckyyadav <yadav.lucky1990@gmail.com>
2023-03-14 00:24:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-file-upload-preview-nokd4b
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-file-upload-preview-nokd4b)
---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular 4';
+  url: any = '';
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event) => {
+        this.url = event.target.result;
+      };
+    }
+  }
+}
